842880517c2f19f05cd6c43b5afa6c71d40007a7
jayce288 <wa594@naver.com>
2021-03-15 23:25:39 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1.Deposit to Account");
+		System.out.println("2.Withdraw to Account");
+		System.out.println("3.Edit Account");
+		System.out.println("4.Account Balance");
+		System.out.println("5.Exit");
+	}
+
+}
