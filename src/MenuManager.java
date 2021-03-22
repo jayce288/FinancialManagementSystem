ffc7ca761e7ac61475382fe984fbce01aee5cf05
jayce288 <wa594@naver.com>
2021-03-23 03:29:26 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,51 @@
+import java.util.Scanner;
 
 public class MenuManager {
-
 	public static void main(String[] args) {
-		System.out.println("1.Deposit to Account");
-		System.out.println("2.Withdraw to Account");
-		System.out.println("3.Edit Account");
-		System.out.println("4.Account Balance");
-		System.out.println("5.Exit");
+		Scanner Input = new Scanner(System.in);
+		int num = 4;
+		
+		while (num != 5) {
+			System.out.println("*** Financial Management System Menu***");
+			System.out.println("1.Deposit to Account");
+			System.out.println("2.Withdraw to Account");
+			System.out.println("3.Account Managemnet");
+			System.out.println("4.Show a Menu");
+			System.out.println("5.Exit");
+			System.out.println("Select one number between 1-5:");
+			num = Input.nextInt();
+			if (num == 1) {
+				DeposittoAccount();
+			}
+			else if (num == 2) {
+				WithdrawtoAccount();
+			}
+			else if (num == 3) {
+				AccountManagemnet();
+			}
+			else {
+				continue;
+			}
+		}
 	}
-
+	
+	public static void DeposittoAccount() {
+		Scanner Input = new Scanner(System.in);
+		System.out.print("Deposit:");
+		int Deposit = Input.nextInt();
+	}
+	
+	public static void WithdrawtoAccount() {
+		Scanner Input = new Scanner(System.in);
+		System.out.print("Withdraw:");
+		int Withdraw = Input.nextInt();
+	}
+	
+	public static void AccountManagemnet() {
+		Scanner Input = new Scanner(System.in);
+		System.out.println("Today Deposit:");
+		System.out.println("Today Withdraw:");
+		System.out.println("Current Balance:");
+	}
 }
+
